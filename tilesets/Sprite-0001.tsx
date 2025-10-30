be37122cfe50351a61ecbcb4af7dc1c7bdf06e7b
Dian-huh <diandiandotdot666@gmail.com>
2025-10-30 22:45:10 +0800

    Add files via upload

---
+++ tilesets/Sprite-0001.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Sprite-0001" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="Sprite-0001.gif" width="320" height="320"/>
+</tileset>
---
+++ tilesets/tilset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tilset" tilewidth="32" tileheight="32" tilecount="638" columns="22">
+ <image source="tilset.png" width="735" height="945"/>
+</tileset>
